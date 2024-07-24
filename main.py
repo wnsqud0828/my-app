4e1a2ba726cf5d16eb181636c6a18f15e940a676
wnsqud0828 <s230220@baeksin.ms.kr>
2024-07-24 11:33:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import koreanize-matplotlib
+import koreanize_matplotlib
 
 file_path = '202406_202406_연령별인구현황_월간 (2).csv'
 data = pd.read_csv(file_path, encoding='euc-kr')
